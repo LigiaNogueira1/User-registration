a34adc6f43e0118cb823981b6652abc07ecf2262
LigiaNogueira1 <109001008+LigiaNogueira1@users.noreply.github.com>
2022-07-23 12:51:12 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,6 +1,3 @@
-#Faça um programa que leia um nome de usuário e a sua senha e
-#não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
-
 print("Wellcome to Lígia's code!")
 Login=(str(input("Type your login: ")))
 Password=(str(input("Type your password: ")))
@@ -12,10 +9,3 @@ while Login == Password:
 
 else:
     print("Registration Sucessful")
-
-
-
-#input("Digite sua senha")
-#nota=float(input("informe um numero de 0 a 10: "))
-#while (nota>10) or (nota<0):
-#	nota=float(input("informe um numero de 0 a 10: "))
